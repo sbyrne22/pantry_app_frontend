837d565952268ffc8937f074791c941b7a53e78f
Sean Byrne <spickles@Seans-iMac.local>
2018-04-14 12:24:55 -0400

    Get and Post routes for Foods are working on the front-end

---
+++ public/javascript/app.js
@@ -0,0 +1,37 @@
+const app = angular.module('PantryApp', []);
+
+app.controller('MainController', ['$http', function($http){
+	// Models. Initial state
+	this.hello = 'oh hai!';
+  this.foods = [];
+  this.formData = {};
+  console.log("What!");
+
+	const GetFoods = () => {
+	  $http({
+	    method: 'GET',
+	    url: 'http://localhost:3000/foods',
+	  }).then(response => {
+	    console.log("response: ", response);
+	    this.foods = response.data.foods;
+	  }).catch(reject => {
+	    console.log("reject ", reject);
+	  });
+	};
+
+	this.addFood = () => {
+    console.log("Form Data ", this.formData);
+		$http({
+			method: 'Post',
+			url: 'http://localhost:3000/storages/' + this.storage + '/containers/' + this.container + '/foods',
+			data: this.formData
+		}).then(response => {
+			console.log('response: ', response);
+			this.foods.unshift(response.data);
+		}).catch(reject => {
+			console.log('reject: ', reject);
+		});
+	};
+
+  GetFoods();
+}]);
